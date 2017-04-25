895de130d8e2103aa60533cc1f99c6aa9ae9972e
remimi <remi.millet@brainify.it>
2017-04-25 12:41:17 +0200

    Add doge mode for contact links

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import DogeMode from './doge-mode/doge-mode'
 
 ReactDOM.render(
   <App />,
   document.getElementById('links')
+);
+
+ReactDOM.render(
+    <DogeMode messages={["emoji-love_letter"]}><a href="mailto:hello@meaningful.studio">Contact us</a></DogeMode>,
+    document.getElementById('first-contact')
+);
+
+ReactDOM.render(
+    <DogeMode messages={["emoji-love_letter"]}><a href="mailto:hello@meaningful.studio">hello@meaningful.studio</a></DogeMode>,
+    document.getElementById('footer-contact')
 );
